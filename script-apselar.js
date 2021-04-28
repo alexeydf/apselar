a2c6b41f1a77d6f47865bb2c97fa0cc979b46036
alexeydf <74327997+alexeydf@users.noreply.github.com>
2021-04-28 02:30:37 -0300

    update

--- script-apselar.js
+++ script-apselar.js
@@ -1,6 +1,13 @@
 const $btnMenu = document.querySelector('.barra-menu');
-const $menu = document.querySelector('.menu')
+const $menu = document.querySelector('.menu');
+const $main = document.querySelector('main');
 
 $btnMenu.addEventListener('click', ()=>{
     $menu.classList.toggle('menu-aberto');
 })
+$main.addEventListener('click', ()=>{
+    $menu.classList.remove('menu-aberto');
+})
+/*$btnMenu.addEventListener('click', ()=>{
+    $menu.classList.toggle('menu-aberto');
+})*/

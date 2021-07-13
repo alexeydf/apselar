5ddba8212b8124351a725670b5e51ccf02a5c228
alexeydf <74327997+alexeydf@users.noreply.github.com>
2021-07-13 08:01:13 -0300

    update-banner

--- script-apselar.js
+++ script-apselar.js
@@ -1,3 +1,4 @@
+//animação do menu
 const $btnMenu = document.querySelector('.barra-menu');
 const $menu = document.querySelector('.menu');
 const $main = document.querySelector('main');
@@ -11,3 +12,37 @@ $main.addEventListener('click', ()=>{
 /*$btnMenu.addEventListener('click', ()=>{
     $menu.classList.toggle('menu-aberto');
 })*/
+
+//animação banner
+
+const $img = document.querySelectorAll('.banner-img img');
+const $btnBannerLeft = document.querySelector('.left');
+const $btnBannerRight = document.querySelector('.right');
+
+$btnBannerLeft.addEventListener('click', ()=>{
+    clearInterval(tempoBanner);
+    animacaoBanner();
+})
+$btnBannerRight.addEventListener('click', ()=>{
+    clearInterval(tempoBanner);
+    animacaoBanner();
+})
+$img[0].addEventListener('mouseover', ()=>{
+    $btnBannerLeft.classList.add('btn-banner-aparece');
+})
+
+let n = 0;
+let tempoBanner = null;
+
+tempoBanner = setInterval(()=>{
+    animacaoBanner();
+},10000)
+
+function animacaoBanner(){
+    $img[n].classList.remove('aparecendo');
+    n++
+    if(n >= ($img.length )){
+        n = 0;
+    }
+    $img[n].classList.add('aparecendo');
+}
